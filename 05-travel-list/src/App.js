02d6751daa4c72360339a4bcddb0cc70c1ff6a9f
Do Van Tai <dvtai1987@gmail.com>
2026-06-20 11:55:17 +0700

    80.Thinking about state and lifting state up

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -7,11 +7,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(initialItems);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +25,7 @@ function Logo() {
   return <h1>🌴 Far Away 🎒</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -32,6 +36,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log({ newItem });
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -60,11 +66,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
